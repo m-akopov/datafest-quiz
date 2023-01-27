b3e204582e5b7540f0db3ed0331bc96e20d88bf3
mishaakopov@gmail.com <mishaakopov@gmail.com>
2023-01-27 23:04:38 +0400

    Done quiz without design

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,113 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [showFinalResults, setFinalResults] = useState(false);
+  const [score, setScore] = useState(0);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+
+  const questions = [
+    {
+      text: "What is the capital of America?",
+      options: [
+        { id: 0, text: "New York City", isCorrect: false },
+        { id: 1, text: "Boston", isCorrect: false },
+        { id: 2, text: "Santa Fe", isCorrect: false },
+        { id: 3, text: "Washington DC", isCorrect: true },
+      ],
+    },
+    {
+      text: "What year was the Constitution of America written?",
+      options: [
+        { id: 0, text: "1787", isCorrect: true },
+        { id: 1, text: "1776", isCorrect: false },
+        { id: 2, text: "1774", isCorrect: false },
+        { id: 3, text: "1826", isCorrect: false },
+      ],
+    },
+    {
+      text: "Who was the second president of the US?",
+      options: [
+        { id: 0, text: "John Adams", isCorrect: true },
+        { id: 1, text: "Paul Revere", isCorrect: false },
+        { id: 2, text: "Thomas Jefferson", isCorrect: false },
+        { id: 3, text: "Benjamin Franklin", isCorrect: false },
+      ],
+    },
+    {
+      text: "What is the largest state in the US?",
+      options: [
+        { id: 0, text: "California", isCorrect: false },
+        { id: 1, text: "Alaska", isCorrect: true },
+        { id: 2, text: "Texas", isCorrect: false },
+        { id: 3, text: "Montana", isCorrect: false },
+      ],
+    },
+    {
+      text: "Which of the following countries DO NOT border the US?",
+      options: [
+        { id: 0, text: "Canada", isCorrect: false },
+        { id: 1, text: "Russia", isCorrect: true },
+        { id: 2, text: "Cuba", isCorrect: true },
+        { id: 3, text: "Mexico", isCorrect: false },
+      ],
+    },
+  ];
+
+  
+  function setAnswer(isCorrect: Boolean) {
+     setScore(isCorrect ? score+1: score)
+
+    if(currentQuestionIndex === questions.length -1){
+      setFinalResults(true)
+    }else{
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    }
+  }
+
+  function restartGame(){
+    setCurrentQuestionIndex(0)
+    setFinalResults(false)
+    setScore(0)
+  }
+
+  
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      {showFinalResults ? (
+        // Result
+        <div className='final-results-wrapper'>
+          <h1>Final Results</h1>
+          <h2>{score} out of {questions.length} is correct - ({score/questions.length*100}%)</h2>
+          <button onClick={restartGame}>Restart game</button>
+        </div>
+      ) : (
+        <div>
+          {/* Score */}
+          Current score is {score}
+
+          {/* Question */}
+          <div className='question-section-wrapper'>
+            <span className='progress-text'>Question 1 of 9 </span>
+            <span className='question-text'>{questions[currentQuestionIndex].text}</span>
+
+            <ul className='answers-wrapper'>
+              {
+                questions[currentQuestionIndex].options.map(answer => {
+                  return <li key={answer.id} onClick={() => {
+                    setAnswer(answer.isCorrect)
+                  } }>{answer.text}</li>;
+                })
+              }
+            </ul>
+          </div>
+
+        </div>
+
+      )}
+    </div >
   );
 }
 
